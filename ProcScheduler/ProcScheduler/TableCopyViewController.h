53c414e63c57f6e4e3012458b6a1ca84999008b1
ankykush <anky.kush@gmail.com>
2015-03-31 20:34:51 -0600

    Added TableCopyViewController

---
+++ ProcScheduler/ProcScheduler/TableCopyViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableCopyViewController.h
+//  ProcScheduler
+//
+//  Created by Ankush Jain on 30/03/15.
+//  Copyright (c) 2015 SecureAppTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableCopyViewController : UIViewController
+
+@end
